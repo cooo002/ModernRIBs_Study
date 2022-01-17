9461d7174ef3e3d0822701db1755b2e2e65306b1
teo.kim <cooo002@naver.com>
2022-01-17 23:34:13 +0900

    Add_ 슈퍼페이 리블렛 빌더 생성 및 부몰 리블렛 빌더 로직에 붙이기 완료

--- MiniSuperApp/FinanceHome/FinanceHomeRouter.swift
+++ MiniSuperApp/FinanceHome/FinanceHomeRouter.swift
@@ -1,19 +1,22 @@
 import ModernRIBs
 
 protocol FinanceHomeInteractable: Interactable {
-  var router: FinanceHomeRouting? { get set }
-  var listener: FinanceHomeListener? { get set }
+    var router: FinanceHomeRouting? { get set }
+    var listener: FinanceHomeListener? { get set }
 }
 
 protocol FinanceHomeViewControllable: ViewControllable {
-  // TODO: Declare methods the router invokes to manipulate the view hierarchy.
+    // TODO: Declare methods the router invokes to manipulate the view hierarchy.
 }
 
 final class FinanceHomeRouter: ViewableRouter<FinanceHomeInteractable, FinanceHomeViewControllable>, FinanceHomeRouting {
-  
-  // TODO: Constructor inject child builder protocols to allow building children.
-  override init(interactor: FinanceHomeInteractable, viewController: FinanceHomeViewControllable) {
-    super.init(interactor: interactor, viewController: viewController)
-    interactor.router = self
-  }
+    private let superPayDashboardBuildable: SuperPayDashboardBuildable
+    // TODO: Constructor inject child builder protocols to allow building children.
+    init(interactor: FinanceHomeInteractable,
+                  viewController: FinanceHomeViewControllable,
+                  superPayDashboardBuildable: SuperPayDashboardBuildable) {
+        self.superPayDashboardBuildable = superPayDashboardBuildable
+        super.init(interactor: interactor, viewController: viewController)
+        interactor.router = self
+    }
 }
